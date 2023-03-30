1601441df971442513202bc4009a2d1da934fce1
warlleyfdz <warlleyfdz@hotmail.com>
2023-03-30 20:59:17 -0300

    inicialiazacao de exercicio

---
+++ src/main/java/com/primeirotranpodev/exececioObjetos/ExececioObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotranpodev.exececioObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExececioObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExececioObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotranpodev/exececioObjetos/ExececioObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotranpodev.exececioObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExececioObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
